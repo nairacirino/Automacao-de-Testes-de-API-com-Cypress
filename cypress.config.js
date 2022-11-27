57c8484bd0b0c01e35a3793bae01d70320b4a453
Naira Cirino <nacirino@gmail.com>
2022-11-27 15:45:48 -0300

    Packages updates and configurations

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: 'cypress/api/**/*.{js, jsx, ts, tsx}',
+    specPattern: 'cypress/api/**/**/*.{js, jsx, ts, tsx}',
     env: {
       wooCommerce: 'https://cena.reset.cwi.com.br/index.php/wp-json/wc/v3',
       productReview: '/products/reviews'
