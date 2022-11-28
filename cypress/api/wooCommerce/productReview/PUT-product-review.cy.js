271d40bab3942eb92529f6de2042ea330a37e0e4
Naira Cirino <nacirino@gmail.com>
2022-11-27 21:19:11 -0300

    Includes acceptance tests to the PUT method of the Product Review API

--- cypress/api/wooCommerce/productReview/PUT-product-review.cy.js
+++ cypress/api/wooCommerce/productReview/PUT-product-review.cy.js
@@ -39,13 +39,98 @@ describe('Smoke Test', () => {
 
             return productReviewWooCommerceSchema.validateAsync(response.body),
 
-            cy.deleteProductReviewWooCommerce(
-                tokenFixture.token,
-                id,
-                productReviewFixture.deleteReview.force
-            )
+                cy.deleteProductReviewWooCommerce(
+                    tokenFixture.token,
+                    id,
+                    productReviewFixture.deleteReview.force
+                )
         })
 
     })
 
+})
+
+describe('Acceptance Test', () => {
+
+    it('Should return 401 when updating a product review without a valid token', () => {
+        let reviewer_email = faker.internet.email()
+        cy.postProductReviewWooCommerce(
+            tokenFixture.token,
+            productReviewFixture.validReview.product_id,
+            productReviewFixture.validReview.review,
+            productReviewFixture.validReview.reviewer,
+            reviewer_email,
+            productReviewFixture.validReview.rating
+        ).then((postResponse) => {
+            let id = postResponse.body.id
+            cy.putProductReviewWooCommerce(
+                tokenFixture.invalidToken,
+                id,
+                productReviewFixture.changeValidReview.product_id,
+                productReviewFixture.changeValidReview.review,
+                productReviewFixture.changeValidReview.reviewer,
+                productReviewFixture.changeValidReview.reviewer_email,
+                productReviewFixture.changeValidReview.rating
+            ).then((updateResponse) => {
+                expect(updateResponse.status).to.eq(statusFixture.unauthorized)
+                cy.getProductReviewWooCommerce(
+                    '/' + id,
+                    tokenFixture.token
+                ).then((getResponse) => {
+                    expect(getResponse.status).to.eq(statusFixture.ok)
+                    expect(getResponse.body.id).to.eq(id)
+                    expect(getResponse.body.product_id).to.eq(productReviewFixture.validReview.product_id)
+                    expect(getResponse.body.reviewer).to.eq(productReviewFixture.validReview.reviewer)
+                    expect(getResponse.body.reviewer_email).to.eq(reviewer_email)
+                    expect(getResponse.body.rating).to.eq(productReviewFixture.validReview.rating)
+                })
+                cy.deleteProductReviewWooCommerce(
+                    tokenFixture.token,
+                    id,
+                    productReviewFixture.deleteReview.force
+                )
+            })
+        })
+    })
+
+    it.only('Should return 404 when updating a product review informing wrong product_id', () => {
+        let reviewer_email = faker.internet.email()
+        cy.postProductReviewWooCommerce(
+            tokenFixture.token,
+            productReviewFixture.validReview.product_id,
+            productReviewFixture.validReview.review,
+            productReviewFixture.validReview.reviewer,
+            reviewer_email,
+            productReviewFixture.validReview.rating
+        ).then((postResponse) => {
+            let id = postResponse.body.id
+            cy.putProductReviewWooCommerce(
+                tokenFixture.token,
+                id,
+                999,
+                productReviewFixture.changeValidReview.review,
+                productReviewFixture.changeValidReview.reviewer,
+                productReviewFixture.changeValidReview.reviewer_email,
+                productReviewFixture.changeValidReview.rating
+            ).then((updateResponse) => {
+                expect(updateResponse.status).to.eq(statusFixture.not_found)
+                cy.getProductReviewWooCommerce(
+                    '/' + id,
+                    tokenFixture.token
+                ).then((getResponse) => {
+                    expect(getResponse.status).to.eq(statusFixture.ok)
+                    expect(getResponse.body.id).to.eq(id)
+                    expect(getResponse.body.product_id).to.eq(productReviewFixture.validReview.product_id)
+                    expect(getResponse.body.reviewer).to.eq(productReviewFixture.validReview.reviewer)
+                    expect(getResponse.body.reviewer_email).to.eq(reviewer_email)
+                    expect(getResponse.body.rating).to.eq(productReviewFixture.validReview.rating)
+                })
+                cy.deleteProductReviewWooCommerce(
+                    tokenFixture.token,
+                    id,
+                    productReviewFixture.deleteReview.force
+                )
+            })
+        })
+    })
 })
